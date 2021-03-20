2d48491191e37ba200fdd0e9fcabbf85b3cb3812
Nakyung-Lee <68135400+Nakyung-Lee@users.noreply.github.com>
2021-03-20 13:27:20 +0900

    Update calculation.java

--- calculation.java
+++ calculation.java
@@ -9,13 +9,17 @@ public class HelloWorld {
 		int result;
 
 		Scanner s=new Scanner(System.in);
+		//Scanner의 객체 생성
+		
 		System.out.print("첫번째 값을 입력하세요 ==> ");
-		a=s.nextInt();
+		a=s.nextInt(); // 입력
+		
 		System.out.print("두번째 값을 입력하세요 ==> ");
 		b=s.nextInt();
 		
 		result=a+b;
-		System.out.println(a+"+"+b+"="+result);
+		System.out.println(a+"+"+b+"="+result); // 출력 
+		//연산자 출력 할 때 " " 안에 작성
 		result=a-b;
 		System.out.println(a+"-"+b+"="+result);
 		result=a*b;
