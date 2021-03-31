3d246e1b7b820de92d00f4a302733f070a7803d3
Nakyung-Lee <68135400+Nakyung-Lee@users.noreply.github.com>
2021-03-31 10:19:34 +0900

    Update 문자열(연습).java

--- 문자열(연습).java
+++ 문자열(연습).java
@@ -16,3 +16,35 @@ public class HelloWorld {
 	}
 
 }
+
+package javabook.ch2;
+
+import java.util.Scanner;
+
+public class HelloWorld {	
+	static int func() {
+		return (int)(Math.random()*100);
+	}
+	public static void main(String[] args) {
+		
+		Scanner s=new Scanner(System.in);
+
+		
+		int a[]=new int[6];//int형 배열 생성
+		System.out.println("Lotto 숫자는 ? ");
+		for(int i=0;i<6;i++) {
+			a[i]=func();
+		}
+		for(int i=0;i<5;i++) {
+			for(int j=i+1;j<6;j++) {
+				if(a[i]==a[j]) {
+					a[i]=func();
+				}
+			}
+		}
+		for(int i=0;i<6;i++) {
+			System.out.print(a[i]+" " );
+		}
+	}
+
+}
