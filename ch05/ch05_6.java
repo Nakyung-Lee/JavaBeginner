1072ffcf2a5b37531a383480ae3db0d19afdb27a
Nakyung-Lee <68135400+Nakyung-Lee@users.noreply.github.com>
2021-10-05 14:49:46 +0900

    Create ch05_6.java

---
+++ ch05/ch05_6.java
@@ -0,0 +1,26 @@
+package chapter03;
+
+import java.util.Scanner;
+
+public class ch05_6 {
+
+	public static void main(String[] args) {
+		System.out.print("숫자를 여러 개 입력 : ");
+		Scanner s = new Scanner(System.in);
+		String str=s.next();
+		for(int i=0;i<str.length();i++) {
+			int n=(str.charAt(i)-'0');
+			for(int j=0;j<n;j++) {
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+	}
+
+}
+
+# 숫자를 여러 개 입력 : 5914
+# *****
+# *********
+# *
+# ****
